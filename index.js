29e97963d2864d76f55ef722cdcfd66a94f89f4e
prajwalnawale62-droid <prajwalnawale62@gmail.com>
2026-03-17 03:14:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function startClient() {
 
 startClient();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
