3da0ed79de69663f720af096d7297efd40d0b362
prajwalnawale62-droid <prajwalnawale62@gmail.com>
2026-03-16 18:58:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ function startClient() {
   client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
