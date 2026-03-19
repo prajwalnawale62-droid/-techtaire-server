73c9d3205a628a2324907270fc6a4444e3abb250
prajwalnawale62-droid <prajwalnawale62@gmail.com>
2026-03-19 15:25:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,40 +91,15 @@ app.post('/bulk-send', async (req, res) => {
 function startClient() {
   client = new Client({
     authStrategy: new NoAuth(),
-    webVersion: '2.3000.1015901503',
-    webVersionCache: {
-      type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1015901503.html'
-    },
     puppeteer: {
       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
-      handleSIGINT: false,
-      handleSIGTERM: false,
-      handleSIGHUP: false,
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
         '--no-zygote',
-        '--single-process',
-        '--disable-gpu',
-        '--disable-extensions',
-        '--disable-background-networking',
-        '--disable-default-apps',
-        '--disable-sync',
-        '--disable-translate',
-        '--hide-scrollbars',
-        '--metrics-recording-only',
-        '--mute-audio',
-        '--safebrowsing-disable-auto-update',
-        '--ignore-certificate-errors',
-        '--ignore-ssl-errors',
-        '--ignore-certificate-errors-spki-list'
-      ],
-      ignoreHTTPSErrors: true,
-      timeout: 120000
+        '--disable-gpu'
+      ]
     }
   });
 
